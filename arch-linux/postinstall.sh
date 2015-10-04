5db1fb0a433898fc873aa7200cfb3396919cf36e
Markus Felten <Markus.Felten@gmx.de>
2015-10-04 16:40:23 +0200

    config ssh/sshd

--- arch-linux/postinstall.sh
+++ arch-linux/postinstall.sh
@@ -1,14 +1,3 @@
 pacman -Syu
 pacman -S mercurial
 pacman -U mercurial-3.3.2-1-armv7h.pkg.tar.xz
-
-pacman -S go
-
-cat <EOF >>/etc/ssh/sshd_config
-PermitRootLogin yes
-PubkeyAcceptedKeyTypes +ssh-dss
-EOF
-
-cat <EOF >>/etc/ssh/ssh_config
-PubkeyAcceptedKeyTypes +ssh-dss
-EOF
