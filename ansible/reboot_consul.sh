b8643b799a65d75a4451732e50f433e852291edb
Markus Felten <Markus.Felten@gmx.de>
2016-02-15 19:56:09 +0100

    feat: rebuild consul cluster

---
+++ ansible/reboot_consul.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+servers="odroid1 odroid2 odroid3 odroid4 odroid5"
+
+for i in $servers
+do
+  ssh -l root -A $i "systemctl stop consul"
+  ssh -l root -A $i "rm -rf /var/consul/*"
+done
+
+for i in $servers
+do
+  ssh -l root -A $i "systemctl start consul"
+done
+
+for i in $servers
+do
+  ssh -l root -A $i "consul info|grep state"
+done
+for i in $servers
+do
+  ssh -l root -A $i "systemctl stop kronos-cluster-node"
+  ssh -l root -A $i "systemctl start kronos-cluster-node"
+done
+
+exit
