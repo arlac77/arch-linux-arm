e2e8ca96327fda2d18ba30521f10d47aaf7c6359
Markus Felten <Markus.Felten@gmx.de>
2017-01-01 00:27:49 +0100

    feat: rpi

--- install/install.sh
+++ install/install.sh
@@ -3,15 +3,15 @@
 HOSTNAME=$1
 
 read HOSTNAME IP GATEWAY MACADDR PLATFORM <<< $(grep $HOSTNAME<<EOF
-ordoid0  10.0.6.0  10.0.0.2 0:1e:6:10:06:0  odroid-xu3
-ordoid1  10.0.6.1  10.0.0.2 0:1e:6:10:06:1  odroid-xu4
-ordoid2  10.0.6.2  10.0.0.2 0:1e:6:10:06:2  odroid-xu4
-ordoid3  10.0.6.3  10.0.0.2 0:1e:6:10:06:3  odroid-xu4
-ordoid4  10.0.6.4  10.0.0.2 0:1e:6:10:06:4  odroid-xu4
-ordoid5  10.0.6.5  10.0.0.2 0:1e:6:10:06:5  odroid-xu4
-ordoid11 10.0.6.11 10.0.0.2 0:1e:6:10:06:11 odroid-c2
-pine1    10.0.7.21 10.0.0.2 0:1e:6:10:06:21 pine64
-rpi1     10.0.6.31 10.0.0.2 0:1e:6:10:06:31 rpi-3
+ordoid0  10.0.6.0  10.0.0.2 0:1e:6:10:06:0    odroid-xu3
+ordoid1  10.0.6.1  10.0.0.2 0:1e:6:10:06:1    odroid-xu4
+ordoid2  10.0.6.2  10.0.0.2 0:1e:6:10:06:2    odroid-xu4
+ordoid3  10.0.6.3  10.0.0.2 0:1e:6:10:06:3    odroid-xu4
+ordoid4  10.0.6.4  10.0.0.2 0:1e:6:10:06:4    odroid-xu4
+ordoid5  10.0.6.5  10.0.0.2 0:1e:6:10:06:5    odroid-xu4
+ordoid11 10.0.6.11 10.0.0.2 0:1e:6:10:06:11   odroid-c2
+pine1    10.0.7.21 10.0.0.2 0:1e:6:10:06:21   pine64
+rpi1     10.0.6.31 10.0.0.2 b8:27:eb:5c:84:bd rpi-3
 EOF
 )
 
