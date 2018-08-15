35ae20e259b6357d09d92d8a7b157c84aef8a7eb
Markus Felten <Markus.Felten@gmx.de>
2018-08-15 10:23:00 +0200

    add pacman-key

--- install/postinstall.sh
+++ install/postinstall.sh
@@ -1,9 +1,9 @@
+pacman-key --init
+pacman-key --populate archlinuxarm
 pacman -Syu
 pacman -S ansible
 
 
-cd /usr/bin;
-rm python
-ln -s python2.7 python
-
-hostnamectl set-hostname odroidx
+#cd /usr/bin;
+#rm python
+#ln -s python2.7 python
