d3f28f886622eae7b8c1b48e0dcfbedc0351e408
Markus Felten <Markus.Felten@gmx.de>
2018-12-20 17:54:26 +0100

    fix: ext2 -> ext4

--- install/install.sh
+++ install/install.sh
@@ -3,17 +3,17 @@
 HOSTNAME=$1
 
 read HOSTNAME IP GATEWAY MACADDR PLATFORM PART_SCHEME <<< $(grep $HOSTNAME<<EOF
-odroid0  10.0.6.0  10.0.0.2 0:1e:6:10:06:0    odroid-xu3  ext2_only
-odroid1  10.0.6.1  10.0.0.2 00:1e:06:31:08:57 odroid-xu3  ext2_only
-odroid2  10.0.6.2  10.0.0.2 0:1e:6:10:06:2    odroid-xu3  ext2_only
-odroid3  10.0.6.3  10.0.0.2 0:1e:6:10:06:3    odroid-xu3  ext2_only
-odroid4  10.0.6.4  10.0.0.2 0:1e:6:10:06:4    odroid-xu3  ext2_only
-odroid5  10.0.6.5  10.0.0.2 0:1e:6:10:06:5    odroid-xu3  ext2_only
-odroid11 10.0.6.11 10.0.0.2 0:1e:6:10:06:11   odroid-c2   ext2_only
-pine1    10.0.6.21 10.0.0.2 0:1e:6:10:06:21   pine64      ext2_only
-pine2    10.0.6.22 10.0.0.2 0:1e:6:10:06:22   pine64      ext2_only
-pine3    10.0.6.23 10.0.0.2 0:1e:6:10:06:23   pine64      ext2_only
-rock1    10.0.6.24 10.0.0.2 3e:2a:57:bf:39:46 rock64      ext2_only
+odroid0  10.0.6.0  10.0.0.2 0:1e:6:10:06:0    odroid-xu3  ext4_only
+odroid1  10.0.6.1  10.0.0.2 00:1e:06:31:08:57 odroid-xu3  ext4_only
+odroid2  10.0.6.2  10.0.0.2 0:1e:6:10:06:2    odroid-xu3  ext4_only
+odroid3  10.0.6.3  10.0.0.2 0:1e:6:10:06:3    odroid-xu3  ext4_only
+odroid4  10.0.6.4  10.0.0.2 0:1e:6:10:06:4    odroid-xu3  ext4_only
+odroid5  10.0.6.5  10.0.0.2 0:1e:6:10:06:5    odroid-xu3  ext4_only
+odroid11 10.0.6.11 10.0.0.2 0:1e:6:10:06:11   odroid-c2   ext4_only
+pine1    10.0.6.21 10.0.0.2 0:1e:6:10:06:21   pine64      ext4_only
+pine2    10.0.6.22 10.0.0.2 0:1e:6:10:06:22   pine64      ext4_only
+pine3    10.0.6.23 10.0.0.2 0:1e:6:10:06:23   pine64      ext4_only
+rock1    10.0.6.24 10.0.0.2 3e:2a:57:bf:39:46 rock64      ext4_only
 rpi1     10.0.6.31 10.0.0.2 b8:27:eb:5c:84:bd rpi-3       vfat_ext2
 EOF
 )
